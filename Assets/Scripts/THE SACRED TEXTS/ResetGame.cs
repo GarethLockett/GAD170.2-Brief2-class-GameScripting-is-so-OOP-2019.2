1efffed7b043e1f1bfc05b3a491e7ca45eb768d4
Gareth Lockett <g.lockett@sae.edu>
2019-11-08 00:39:16 +0800

    Inital commit
    Unity project ready to go.

---
+++ Assets/Scripts/THE SACRED TEXTS/ResetGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetGame : MonoBehaviour
+{
+    private void Update()
+    {
+        // Quick and dirty one-liner to reload the scene (eg use at the end of the battle)
+        if( Input.GetKeyDown( KeyCode.Escape ) == true ){ UnityEngine.SceneManagement.SceneManager.LoadScene( 0 ); }
+    }
+}
